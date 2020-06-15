41ce301cccfeda605a167ea9e94201dd039611b9
Miko <m.klarzuk@uniform-agri.com>
2020-06-16 00:50:29 +0200

    Updates

--- src/js/index.js
+++
@@ -1,7 +0,0 @@
-import greetModule from './greeting';
-import '../sass/main.scss';
-
-document.addEventListener('DOMContentLoaded', () => {
-  document.querySelector('h1').textContent = greetModule.greet();
-  document.querySelector('p').textContent = greetModule.greet2();
-});
