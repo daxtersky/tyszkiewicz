4f8b6fcb2d4a83c73102a381173629430290f267
Miko <m.klarzuk@uniform-agri.com>
2020-06-23 22:44:12 +0200

    First layout attempt

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,11 @@
-import greetModule from './greeting';
+// import greetModule from './greeting';
 import '../sass/main.scss';
 
 document.addEventListener('DOMContentLoaded', () => {
-  document.querySelector('h1').textContent = greetModule.greet();
-  document.querySelector('p').textContent = greetModule.greet2();
+  // document.querySelector('h1').textContent = greetModule.greet();
+  // document.querySelector('p').textContent = greetModule.greet2();
+
+  // "prod1": "parcel build index.html --public-url https://github.com/daxtersky/daxtersky.github.io/tyszkiewicz",
+  // "deploy": "rm -rf dist/ && npm run prod1 && gh-pages -d dist "
+
 });
